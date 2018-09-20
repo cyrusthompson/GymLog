bd804f61bca1aa539746c675a0183b3470a85ffc
cyrusthompson <cyrusthompson@hotmail.com>
2018-09-20 17:08:35 +0100

    test comment more

--- app/src/main/java/com/example/thompson/gymlog/MainActivity.java
+++ app/src/main/java/com/example/thompson/gymlog/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //This is me testing out git hub
+        //This is me testing out git hub !!!!
     }
 }
